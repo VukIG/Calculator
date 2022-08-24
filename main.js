4f08d801a99e1d5caf4848867fc77a5dd33936e3
VukIG <vuk.ignjatovic05@gmail.com>
2022-08-23 19:47:04 -0800

    ne ide

--- main.js
+++ main.js
@@ -2,17 +2,45 @@ const redarmy=document.querySelectorAll(".aa");
 const display=document.querySelector(".display");
 const lastchoice=document.querySelector(".lastchoice");
 
+let num1;
+let num2;
+
+
+function calculate(params) {
+    switch (params) {
+        case '+':
+            return num1+num2;
+        case '-':
+            return num1-num2;
+        case '*':
+            return num1*num2;
+        case '÷':
+            return num1/num2;
+        default:
+            break;
+    }
+}
 
 
 redarmy.forEach(element => {
     let button=element;
     button.addEventListener("click",function () {
-        if(button.innerHTML=='+' || button.innerHTML=='-' || button.innerHTML=='*' || button.innerHTML=='÷' || button.innerHTML=='*' || button.innerHTML=='%'){
-            console.log(button.innerHTML)
-            lastchoice.innerHTML=button.innerHTML;
+        if (button.innerHTML=='AC'){
+            lastchoice.innerHTML='';
+            display.innerHTML='';
+        }
+        if(button.innerHTML=='='){
+            let result=calculate();
+            console.log(result);
+        }
+        else if(button.innerHTML=='+' || button.innerHTML=='-' || button.innerHTML=='*' || button.innerHTML=='÷' || button.innerHTML=='*' || button.innerHTML=='%'){
+            
+            num1=parseFloat(display.innerHTML);
+            console.log(num1);
+            display.innerHTML+=button.innerHTML;
         }
         else{
             display.innerHTML+=button.innerHTML;   
         }  
-    })
+    });
 });
